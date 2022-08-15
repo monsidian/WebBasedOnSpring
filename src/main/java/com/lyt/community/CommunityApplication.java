49c26a48476111b638883c540517b83247082151
LongYutong <403216817@qq.com>
2022-08-15 18:59:35 +0800

    commit 1

--- src/main/java/com/lyt/community/CommunityApplication.java
+++ src/main/java/com/lyt/community/CommunityApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication	//表明该类是一个配置类，一般用于程序入口
 public class CommunityApplication {
-
+	// 1
 	public static void main(String[] args) {
 		SpringApplication.run(CommunityApplication.class, args);
 	}
